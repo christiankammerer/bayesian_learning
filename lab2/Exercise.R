b9c4b444cb67b3b07d2214ef44dfe6e381608618
christiankammerer <ckammerer27@gmail.com>
2025-04-23 11:50:37 +0200

    Implemented 1a and first half of 1b for lab 2

---
+++ lab2/Exercise.R
@@ -0,0 +1,132 @@
+library(asbio)
+library(mvtnorm)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(matlib)
+
+data <- read.csv("temp_linkoping.csv")
+
+beta_prior_f <- function(n, mu0, sigma_sq, omega_0_inv){
+  return(rmvnorm(n, mean = mu0, sigma = sqrt(sigma_sq * omega_0_inv)))
+}
+
+sigma_prior_f <- function(n, v0, sigma_sq_0){
+  rinvchisq(n, df = v0, scale = sigma_sq_0)
+}
+
+joint_prior <- function(n=1, mu0 = c(20, -100, 100), 
+                        omega_0_inv = 0.01 * diag(3), 
+                        v0=1, sigma_sq_0=1){
+  sigma_sq <- sigma_prior_f(1, v0, sigma_sq_0)
+  beta <- beta_prior_f(1, mu0, sigma_sq, omega_0_inv)
+  return(beta)
+}
+
+beta_draws <- sapply(1:100, joint_prior)
+
+
+
+# Transpose to make each row one set of coefficients
+beta_draws_t <- t(beta_draws)
+
+x_vals <- seq(1, 365, 1)/365
+
+# Build data frame of predictions
+curve_data <- lapply(1:nrow(beta_draws_t), function(i) {
+  beta <- beta_draws_t[i, ]
+  data.frame(
+    x = x_vals,
+    y = beta[1] + beta[2]*x_vals + beta[3]*x_vals^2,
+    curve = paste0("Draw_", i)
+  )
+}) %>% bind_rows()
+
+p <- ggplot(curve_data, aes(x = x, y = y, group = curve, color = curve)) +
+  geom_line(alpha = 0.7) +
+  labs(title = "Regression Curves from Prior Draws",
+       x = "x", y = "y") +
+  theme_minimal() +
+  theme(legend.position = "none")  # optional, removes legend
+
+print(p)
+
+# Returns posterior parameters (mu_n, omega_n, sigma_sq_n, vn)
+get_posterior_parameters <- function(mu0, omega0, sigma_sq_0, v0, y, X) {
+  vn <- v0 + length(y)
+  XtX <- t(X) %*% X
+  omega_n <- XtX + omega0
+  beta_hat <- solve(XtX) %*% t(X) %*% y
+  mu_n <- solve(omega_n) %*% (XtX %*% beta_hat + omega0 %*% mu0)
+  sigma_sq_n <- as.numeric(
+    (v0 * sigma_sq_0 + t(y) %*% y + t(mu0) %*% omega0 %*% mu0 - t(mu_n) %*% omega_n %*% mu_n) / vn
+  )
+  
+  return(list(
+    mu_n = mu_n,
+    omega_n = omega_n,
+    sigma_sq_n = sigma_sq_n,
+    vn = vn
+  ))
+}
+
+sigma_posterior_f <- function(n = 1, posterior_params){
+  with(posterior_params, {
+    return(rinvchisq(n, df = vn, scale = sigma_sq_n))
+  })
+}
+
+beta_posterior_f <- function(n = 1, posterior_params) {
+  with(posterior_params, {
+    # Sample sigma
+    sigma_sq_samples <- sigma_posterior_f(n = n, posterior_params)
+    
+    # Sample beta conditional for each sigma
+    beta_samples <- t(sapply(sigma_sq_samples, function(sigma_sq) {
+      rmvnorm(1, mean = mu_n, sigma = sigma_sq * solve(omega_n))
+    }))
+    
+    return(list(beta = beta_samples, sigma_sq = sigma_sq_samples))
+  })
+}
+
+marginal_beta <- function(n, mu, Sigma, df) {
+  p <- length(mu)
+  chi_samples <- rchisq(n, df)
+  Z <- matrix(rnorm(n * p), nrow = n)  # standard normal
+  t_samples <- sweep(Z, 1, sqrt(chi_samples / df), FUN = "/") %*% chol(Sigma)
+  sweep(t_samples, 2, mu, FUN = "+")
+}
+
+X <- data$time
+y <- data$temp
+
+X <- cbind(1, X, X^2)
+posterior_params <- get_posterior_parameters(mu0 = c(20, -100, 100), 
+                                             omega0 = 0.01 * diag(3), 
+                                             sigma_sq_0 = 1, v0 = 1,
+                                             y, X)
+
+posterior_draws <- beta_posterior_f(n = 1000, posterior_params)
+beta_posterior_draws <- posterior_draws$beta
+sigma_posterior_draws <- posterior_draws$sigma
+
+beta_df <- as.data.frame(beta_posterior_draws)
+beta_df$beta0 <- beta_df$V1
+beta_df$beta1 <- beta_df$V2
+beta_df$beta2 <- beta_df$V3
+
+beta_long <- tidyr::gather(beta_df, key = "Beta", value = "Value", beta0, beta1, beta2)
+
+# Plot overlapping histograms
+p <- ggplot(beta_long, aes(x = Value, fill = Beta)) +
+  geom_histogram(alpha = 0.5, position = "identity", bins = 200) + 
+  scale_fill_manual(values = c("red", "blue", "green")) +
+  labs(title = "Overlapping Histograms of Betas",
+       x = "Beta values", 
+       y = "Frequency") +
+  theme_minimal() +
+  theme(legend.title = element_blank())
+print(p)
+
+p <- hist(sigma_posterior_draws, col =  "red")
